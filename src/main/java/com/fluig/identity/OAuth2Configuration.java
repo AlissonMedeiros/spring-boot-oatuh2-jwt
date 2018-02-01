4148a81b6c0ae017a4289a372fa12bbc09cb6bc6
Alisson Medeiros <alisson.medeiros@gmail.com>
2018-02-01 13:31:20 -0200

    Alterando para jdbc

--- src/main/java/com/fluig/identity/OAuth2Configuration.java
+++ src/main/java/com/fluig/identity/OAuth2Configuration.java
@@ -1,15 +1,13 @@
 package com.fluig.identity;
 
-import com.google.common.collect.Lists;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,37 +17,27 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 @EnableAuthorizationServer
 @EnableResourceServer
 public class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("acme") // clientId
-                .secret("acmesecret") // clientSecret
-                .scopes("openid")
-                .autoApprove(true)
-                .authorities("READ", "WRITE")
-                .authorizedGrantTypes("implicit", "refresh_token", "password", "authorization_code", "client_credentials")
-                .redirectUris("http://notes.coding.me");
+        clients.jdbc(dataSource);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        List<UserDetails> userDetailsList = new ArrayList<>();
-        userDetailsList.add(new User("user","user", Lists.newArrayList(new SimpleGrantedAuthority("READ"))));
-        userDetailsList.add(new User("admin","admin",Lists.newArrayList(new SimpleGrantedAuthority("READ"),new SimpleGrantedAuthority("WRITE"))));
-
         endpoints.tokenStore(tokenStore())
                 .tokenEnhancer(jwtTokenEnhancer())
                 .authenticationManager(authenticationManager)
-                .userDetailsService(new InMemoryUserDetailsManager(userDetailsList));
+                .userDetailsService(new JdbcUserDetailsManager());
     }
 
     @Autowired
